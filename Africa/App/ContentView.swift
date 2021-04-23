697fcd753a0d8b2926b625ddac95b4ca95d540ce
Samuel Brehm <samuelbrehm@gmail.com>
2021-04-23 07:03:19 -0300

    Finish complex layout grid

--- Africa/App/ContentView.swift
+++ Africa/App/ContentView.swift
@@ -15,7 +15,29 @@ struct ContentView: View {
     
     @State private var isGridViewActive: Bool = false
     
-    let gridLayout: [GridItem] = Array(repeating: GridItem(.flexible()), count: 2)
+    @State private var gridLayout: [GridItem] = [ GridItem(.flexible()) ]
+    @State private var gridColumn: Int = 1
+    @State private var toolbarIcon: String = "square.grid.2x2"
+    
+    // MARK: - FUNCTIONS
+    
+    func gridSwitch() {
+        gridLayout = Array(repeating: .init(.flexible()), count: gridLayout.count % 3 + 1)
+        gridColumn = gridLayout.count
+        print("Grid number: \(gridColumn)")
+        
+        //TOOLBAR IMAGE
+        switch gridColumn {
+        case 1:
+            toolbarIcon = "square.grid.2x2"
+        case 2:
+            toolbarIcon = "square.grid.3x2"
+        case 3:
+            toolbarIcon = "rectangle.grid.1x2"
+        default:
+            toolbarIcon = "square.grid.2.2"
+        }
+    }
 
     //MARK: - BODY
     var body: some View {
@@ -42,8 +64,8 @@ struct ContentView: View {
                                 } //: LINK
                             } //: LOOP
                         } //: GRID
-                        .padding(10)
                         .animation(.easeIn)
+                        .padding(10)
                     } //: SCROLL
                 } //: CONDITION
             } //: GROUP
@@ -65,8 +87,9 @@ struct ContentView: View {
                             print("Grid view is activated")
                             isGridViewActive = true
                             haptics.impactOccurred()
+                            gridSwitch()
                         }, label: {
-                            Image(systemName: "square.grid.2x2")
+                            Image(systemName: toolbarIcon)
                                 .font(.title2)
                                 .foregroundColor(isGridViewActive ? .accentColor : .primary)
                         })
